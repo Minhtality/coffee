1f537e45ab87fbd4289b17f54ef6557671baf93b
Minh Tran <mtran1712@gmail.com>
2023-03-06 22:32:31 -0800

    removed usless information from success page

--- frontend/src/pages/success/index.jsx
+++ frontend/src/pages/success/index.jsx
@@ -1,35 +1,35 @@
-import { useRouter } from "next/router";
-import { formatPrice } from "@/packages/utils";
-import Image from "next/image";
-import * as Styled from "./index.styled";
+import { useRouter } from 'next/router';
+import { formatPrice } from '@/packages/utils';
+import Image from 'next/image';
+import * as Styled from './index.styled';
 
-const stripe = require("stripe")(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
 export async function getServerSideProps(params) {
-  const order = await stripe.checkout.sessions.retrieve(
-    params.query.session_id,
-    {
-      expand: ["line_items"],
-    }
-  );
+	const order = await stripe.checkout.sessions.retrieve(
+		params.query.session_id,
+		{
+			expand: ['line_items'],
+		}
+	);
 
-  return { props: { order } };
+	return { props: { order } };
 }
 
 export default function Success({ order }) {
-  const route = useRouter();
-  console.log(order);
-  return (
-    <Styled.SuccessContainer
-      initial={{ opacity: 0, scale: 0.7 }}
-      animate={{ opacity: 1, scale: 1 }}
-      transition={{ duration: 0.5 }}
-    >
-      <h2>Thank you for your order, {order.customer_details.name}</h2>
-      <h3>A confirmation email has been sent to: </h3>
-      <h3>{order.customer_details.email}</h3>
-      <Styled.OrderInformation>
-        <Styled.Card>
+	const route = useRouter();
+	console.log(order);
+	return (
+		<Styled.SuccessContainer
+			initial={{ opacity: 0, scale: 0.7 }}
+			animate={{ opacity: 1, scale: 1 }}
+			transition={{ duration: 0.5 }}
+		>
+			<h2>Thank you for your order, {order.customer_details.name}</h2>
+			{/* <h3>A confirmation email has been sent to: </h3>
+			<h3>{order.customer_details.email}</h3> */}
+			<Styled.OrderInformation>
+				{/* <Styled.Card>
           <h4>Address</h4>
           {Object.entries(order.customer_details.address).map(
             ([key, value]) =>
@@ -39,27 +39,25 @@ export default function Success({ order }) {
                 </p>
               )
           )}
-        </Styled.Card>
-        <Styled.Card>
-          <h4>Products</h4>
-          {order.line_items.data.map((item) => (
-            <Styled.LineItem key={item.id}>
-              <p>{item.description}</p>
-              <p>Price: {formatPrice(item.amount_total / 100)}</p>
-              <p>Qty: {item.quantity}</p>
-            </Styled.LineItem>
-          ))}
-        </Styled.Card>
-      </Styled.OrderInformation>
-      <Styled.Back onClick={() => route.push("/")}>
-        Continue Shopping
-      </Styled.Back>
-      <Image
-        src="/images/goodsoup.gif"
-        alt="Good Soup"
-        width={400}
-        height={300}
-      />
-    </Styled.SuccessContainer>
-  );
+        </Styled.Card> */}
+				<Styled.Card>
+					<h4>Your drinks will be made soon!</h4>
+					{/* {order.line_items.data.map((item) => (
+						<Styled.LineItem key={item.id}>
+							<p>{item.description}</p>
+						</Styled.LineItem>
+					))} */}
+				</Styled.Card>
+			</Styled.OrderInformation>
+			<Styled.Back onClick={() => route.push('/')}>
+				Continue Shopping
+			</Styled.Back>
+			<Image
+				src="/images/goodsoup.gif"
+				alt="Good Soup"
+				width={400}
+				height={300}
+			/>
+		</Styled.SuccessContainer>
+	);
 }
--- frontend/src/pages/success/index.styled.jsx
+++ frontend/src/pages/success/index.styled.jsx
@@ -1,54 +1,54 @@
-import styled from "styled-components";
-import { motion } from "framer-motion";
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
 export const SuccessContainer = styled(motion.div)`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin: 0 auto;
-  width: 70%;
-  background-color: #fff;
-  border-radius: 20px;
-  margin-bottom: 1rem;
-  text-align: center;
-  padding: 3rem;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	margin: 0 auto;
+	width: 70%;
+	background-color: #fff;
+	border-radius: 20px;
+	margin-bottom: 1rem;
+	text-align: center;
+	padding: 3rem;
+
+	h2,
+	h3 {
+		margin-bottom: 1rem;
+	}
 `;
 
 export const OrderInformation = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  text-align: left;
-  width: 100%;
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	text-align: left;
+	width: 100%;
 `;
 export const Back = styled.button`
-  background: var(--primary);
-  color: white;
-  width: 100%;
-  font-weight: 500;
-  padding: 1rem 2rem;
-  cursor: pointer;
-  width: 200px;
-  margin: 1rem auto;
+	background: var(--primary);
+	color: white;
+	width: 100%;
+	font-weight: 500;
+	padding: 1rem 2rem;
+	cursor: pointer;
+	width: 200px;
+	margin: 1rem auto;
 `;
 
 export const Card = styled.div`
-  margin: 1rem;
-  padding: 1rem;
-  font-size: 1rem;
-  h4 {
-    margin-bottom: 1rem;
-  }
+	font-size: 1rem;
 `;
 
 export const LineItem = styled.div`
-  margin: 1rem 0;
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-gap: 5px;
+	margin: 1rem 0;
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr;
+	grid-gap: 5px;
 
-  p {
-    margin: 0;
-  }
+	p {
+		margin: 0;
+	}
 `;

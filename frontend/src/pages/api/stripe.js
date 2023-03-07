19d0d3b15e7967738bc86b67793d0133a970d75d
Minh Tran <mtran1712@gmail.com>
2023-03-06 21:49:00 -0800

    added success page after checkout

--- frontend/src/pages/api/stripe.js
+++ frontend/src/pages/api/stripe.js
@@ -8,14 +8,30 @@ const handler = async (req, res) => {
       const { cartItems } = req.body;
 
       const session = await stripe.checkout.sessions.create({
+        success_url: `${req.headers.origin}/success?&session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${req.headers.origin}/cancel`,
         submit_type: "pay",
         payment_method_types: ["card"],
         mode: "payment",
+        allow_promotion_codes: true,
         shipping_address_collection: {
           allowed_countries: ["US"],
         },
+        shipping_options: [
+          {
+            shipping_rate: "shr_1MimRCHj9x82W7wK5yCH2Rer",
+          },
+          {
+            shipping_rate: "shr_1MimEwHj9x82W7wKJ5pVUZFQ",
+          },
+        ],
         line_items: cartItems.map((item) => ({
           quantity: item.quantity,
+          adjustable_quantity: {
+            enabled: true,
+            minimum: 1,
+            maximum: 10,
+          },
           price_data: {
             currency: "usd",
             product_data: {
@@ -25,8 +41,6 @@ const handler = async (req, res) => {
             unit_amount: item.price * 100,
           },
         })),
-        success_url: `${req.headers.origin}/success`,
-        cancel_url: `${req.headers.origin}/cancel`,
       });
 
       res.status(200).json(session);

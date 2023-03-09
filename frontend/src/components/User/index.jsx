3418b3cdb3011424937eda176ebb42380254df76
Minh Tran <mtran1712@gmail.com>
2023-03-08 22:45:18 -0800

    added user profile

--- frontend/src/components/User/index.jsx
+++ frontend/src/components/User/index.jsx
@@ -1,17 +1,34 @@
 import { FaUserCircle } from "react-icons/fa";
 import { useRouter } from "next/router";
 import * as Styled from "./index.styled";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 const User = () => {
   const router = useRouter();
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+  if (!user) {
+    return (
+      <Styled.Profile
+        onClick={() => {
+          router.push("/api/auth/login");
+        }}
+      >
+        <FaUserCircle />
+        <h3>Login</h3>
+      </Styled.Profile>
+    );
+  }
   return (
     <Styled.Profile
       onClick={() => {
-        router.push("/api/auth/login");
+        router.push("/profile");
       }}
     >
-      <FaUserCircle />
-      <h3>Profile</h3>
+      <img src={user?.picture} alt={user.name} />
+      <h3>{user.name}</h3>
     </Styled.Profile>
   );
 };

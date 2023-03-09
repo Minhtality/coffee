5db45e666f47a65a22e826528654440e5e861159
Minh Tran <mtran1712@gmail.com>
2023-03-09 08:19:47 -0800

    chore: fix prettier setting

--- frontend/src/components/product/index.jsx
+++ frontend/src/components/product/index.jsx
@@ -1,20 +1,20 @@
-import * as Styled from './index.styled';
-import Link from 'next/link';
+import * as Styled from "./index.styled";
+import Link from "next/link";
 
 export default function Index({ product, variants }) {
-	const { title, price, image, slug } = product.attributes;
-	const { url, name } = image.data.attributes.formats.small;
-	return (
-		<Styled.Product variants={variants} href={`/product/${slug}`}>
-			<Styled.ProductWrapper>
-				<Styled.ImageWrapper>
-					<img src={url} alt={name} />
-				</Styled.ImageWrapper>
-				<div>
-					<h2>{title}</h2>
-				</div>
-				<h3>{price}</h3>
-			</Styled.ProductWrapper>
-		</Styled.Product>
-	);
+  const { title, price, image, slug } = product.attributes;
+  const { url, name } = image.data.attributes.formats.small;
+  return (
+    <Styled.Product variants={variants} href={`/product/${slug}`}>
+      <Styled.ProductWrapper>
+        <Styled.ImageWrapper>
+          <img src={url} alt={name} />
+        </Styled.ImageWrapper>
+        <div>
+          <h2>{title}</h2>
+        </div>
+        <h3>{price}</h3>
+      </Styled.ProductWrapper>
+    </Styled.Product>
+  );
 }
--- frontend/src/components/product/index.styled.jsx
+++ frontend/src/components/product/index.styled.jsx
@@ -1,30 +1,30 @@
-import styled from 'styled-components';
-import Link from 'next/link';
-import { motion } from 'framer-motion';
+import styled from "styled-components";
+import Link from "next/link";
+import { motion } from "framer-motion";
 
 export const Product = styled(motion(Link))``;
 
 export const ProductWrapper = styled.div`
-	background-color: white;
-	position: relative;
-	display: flex;
-	flex-direction: column;
-	padding: 1.5rem;
-	cursor: pointer;
-	width: 100%;
-	height: 100%;
+  background-color: white;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  padding: 1.5rem;
+  cursor: pointer;
+  width: 100%;
+  height: 100%;
 `;
 
 export const ImageWrapper = styled.div`
-	width: 100%;
-	height: 300px;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	margin: 0 auto;
-	img {
-		width: 100%;
-		height: 100%;
-		object-fit: cover;
-	}
+  width: 100%;
+  height: 300px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
 `;

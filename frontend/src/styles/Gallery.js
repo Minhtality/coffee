1bd09ee68dd98db48c4728749b5740f865fa566b
Minh Tran <mtran1712@gmail.com>
2023-03-08 23:44:07 -0800

    added more animation to product page

--- frontend/src/styles/Gallery.js
+++ frontend/src/styles/Gallery.js
@@ -1,8 +1,9 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
-export const Gallery = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
-  grid-gap: 1.5rem;
-  margin-bottom: 50px;
+export const Gallery = styled(motion.div)`
+	display: grid;
+	grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
+	grid-gap: 1.5rem;
+	margin-bottom: 50px;
 `;

4f51585d84d7aaefbd6280cfcee4fbf15fc64e51
Minhtality <mtran1712@gmail.com>
2024-04-05 07:39:55 +0000

    updated server and gitignore

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1331),
+  url: 'https://api.minhtality.com/',
   app: {
     keys: env.array("APP_KEYS"),
   },

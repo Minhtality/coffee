c89eec2bd5a9deff444f2a9ebade0b935ca34dd0
Minh Tran <mtran1712@gmail.com>
2024-04-13 23:55:08 -0700

    Updated Schema, WIP frontend changes

--- frontend/queries/query.js
+++ frontend/queries/query.js
@@ -1,3 +1,51 @@
+export const PHOTO_QUERY = `
+query{
+    photos{
+      data{
+        attributes{
+          title
+          description
+          category
+          slug
+          image {
+            data{
+              attributes{
+                width
+                height
+                    formats
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const PHOTO_QUERY_BY_CATEGORY = `
+query getPhotosByCategory($category: String!){
+    photos(filters: {category: {eq: $category}}){
+      data{
+        attributes{
+          title
+          description
+          category
+          slug
+          image {
+            data{
+              attributes{
+                width
+                height
+                    formats
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
 export const PRODUCT_QUERY = `
 query{
     products{
